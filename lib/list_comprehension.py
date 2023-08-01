fca8df3e346fb7b67ae1c929e450c18301b40235
Devin Jones <devin.jones21083@gmail.com>
2023-08-01 00:02:56 -0400

    All tests passing

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
-
+    even_list = [n for n in num_list if (n % 2) == 0]
+    if len(even_list) == 0:
+        return list()
+    else:
+        return even_list
 def make_exclamation(sentence_list):
-    pass
+    exclaim_added = [sentence + "!" for sentence in sentence_list]
+    if len(sentence_list) == 0:
+        return list()
+    else:
+        return exclaim_added
